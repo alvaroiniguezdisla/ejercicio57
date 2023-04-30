6e1dd3f3504e0ba3cec37a8ef36d2b9ede4689ba
Álvaro <alvaro.iniguezdisla@usp.ceu.es>
2023-04-30 18:30:40 +0200

    ejercicio 57 terminado

--- src/main/java/org/example/ej_57.java
+++ src/main/java/org/example/ej_57.java
@@ -1,2 +1,20 @@
-package org.example;public class ej_57 {
+package org.example;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ej_57 {
+    public static double mediaArmonicalammbda(List<Double> datos) throws Exception{
+        //return excepcion si la lista esta vacia,contiene algún elemento que sea 0 ó la suma de sus inversos es 0
+        if (datos.isEmpty() || datos.contains(0.0) || datos.stream().mapToDouble(d -> 1/d).sum() == 0) {
+            throw new Exception("Lista vacia ,contiene algun elemento 0 ó la suma de sus inversos es 0");
+
+        }
+        //return la media armonica de los elementos de la lista funcion lambda
+        return datos.size() / datos.stream().mapToDouble(d -> 1 / d).sum();
+
+
+
+
+
+    }
 }
--- src/test/java/org/example/ej57_tests.java
+++ src/test/java/org/example/ej57_tests.java
@@ -1,2 +1,31 @@
-package org.example;public class ej57_tests {
+package org.example;
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+public class ej57_tests {
+    //creamos un test para lista vacia
+    @Test
+    public void testListaVacia() throws Exception {
+        List<Double> datos = new ArrayList<>();
+        assertEquals("Lista vacia ,contiene algun elemento 0 ó la suma de sus inversos es 0",ej_57.mediaArmonicalammbda(datos));
+    }
+    //creamos un test para lista con algun elemento 0
+    @Test
+    public void testListaConCero() throws Exception {
+        List<Double> datos = new ArrayList<>();
+        datos.add(1.0);
+        datos.add(2.0);
+        datos.add(0.0);
+        assertEquals("Lista vacia ,contiene algun elemento 0 ó la suma de sus inversos es 0",ej_57.mediaArmonicalammbda(datos));
+    }
+    //creamos un test para lista con suma de inversos 0
+    @Test
+    public void testListaConSumaInversosCero() throws Exception {
+        List<Double> datos = new ArrayList<>();
+        datos.add(1.0);
+        datos.add(2.0);
+        datos.add(3.0);
+        assertEquals("Lista vacia ,contiene algun elemento 0 ó la suma de sus inversos es 0",ej_57.mediaArmonicalammbda(datos));
+    }
 }
